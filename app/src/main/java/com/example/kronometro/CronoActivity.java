8957aa98686bba8b028de8a957d1aec18ef38631
Adamo Gonçalves Maia <87905208+yKronoos@users.noreply.github.com>
2022-04-14 10:15:37 -0300

    Cronometro terminado

--- app/src/main/java/com/example/kronometro/CronoActivity.java
+++ app/src/main/java/com/example/kronometro/CronoActivity.java
@@ -2,6 +2,8 @@ package com.example.kronometro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -15,6 +17,7 @@ public class CronoActivity extends AppCompatActivity {
     int sec = 0, min = 0, hour = 0;
     Handler handler = new Handler();
     Runnable runnable;
+    int aux=0;
 
 
     @Override
@@ -25,36 +28,45 @@ public class CronoActivity extends AppCompatActivity {
 
     //button start
     public void start(View view) {
+        if(aux==0){
+            aux=1;
 
-        txtViewTime = findViewById(R.id.txtViewTime);
+            txtViewTime = findViewById(R.id.txtViewTime);
 
-        btnStart = findViewById(R.id.btnStart);
-        btnStart.setClickable(false);
+            btnStart = findViewById(R.id.btnStart);
+            btnStart.setText(R.string.stop);
+            btnStart.setBackgroundColor(getResources().getColor(R.color.red));
 
-        handler.postDelayed(runnable = new Runnable() {
-            @Override
-            public void run() {
-                sec++;
-                if(sec == 59){
-                    sec = 0;
-                    min++;
-                }if(min == 59){
-                    min = 0;
-                    hour++;
+            handler.postDelayed(runnable = new Runnable() {
+                @Override
+                public void run() {
+                    sec++;
+                    if(sec == 59){
+                        sec = 0;
+                        min++;
+                    }if(min == 59){
+                        min = 0;
+                        hour++;
+                    }
+                    String time = String.format("%d:%02d:%02d", hour, min, sec);
+                    txtViewTime.setText(time);
+                    handler.postDelayed(this, 1000);
                 }
-                String time = String.format("%d:%02d:%02d", hour, min, sec);
-                txtViewTime.setText(time);
-                handler.postDelayed(this, 1000);
-            }
-        }, 1000);
-
+            }, 1000);
+        }else{
+            stop();
+        }
     }
 
-    //button stop
-    public void stop(View view) {
-        handler.removeCallbacks(runnable);
-        btnStart.setClickable(true);
-
+    //stop
+    public void stop() {
+        if(aux==1){
+            btnStart.setText(R.string.resume);
+            btnStart.setBackgroundColor(getResources().getColor(R.color.green));
+            handler.removeCallbacks(runnable);
+            //volta para start
+            aux = 0;
+        }
     }
 
     //button reset
@@ -64,6 +76,5 @@ public class CronoActivity extends AppCompatActivity {
         min = 0;
         hour = 0;
         txtViewTime.setText("0:00:00");
-        btnStart.setClickable(true);
     }
 }
